97455bf12eb105a3d82390f32530b1b02faece03
Saiteja Mahankali <81941814+saiteja-2731@users.noreply.github.com>
2021-09-29 22:31:36 +0530

    Day 1

--- Strings/MultiplyStrings.cpp
+++ Strings/MultiplyStrings.cpp
@@ -1,6 +1,6 @@
 /*
 Given two numbers represented as strings, return multiplication of the numbers as a string.
-Note: The numbers can be arbitrarily large and are non-negative.
+Note: The numbers can be arbitrarily large and are non-negative
 Note2: Your answer should not have leading zeroes. For example, 00 is not a valid answer. 
 For example,
 given strings "12", "10", your answer should be “120”.
